19fc6a6a1ed840ee4ecb61ed84b4e4772208f3bd
damianSte <stepien.damian.29@gmail.com>
2024-10-16 12:37:21 +0200

    Fixed division by zero

--- Calculator2/src/main/java/org/example/Calculator.java
+++ Calculator2/src/main/java/org/example/Calculator.java
@@ -13,6 +13,8 @@ public class Calculator {
         return a * b;
     }
     public double divide(int a, int b) {
+        if (b == 0)
+            throw new ArithmeticException("Divide by zero");
         return (double) a / b;
     }
 }
